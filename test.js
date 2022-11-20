63900ece3127612d4d82e66c0776dc0feeb16db4
Titus Wormer <tituswormer@gmail.com>
2022-11-20 11:36:56 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {speakers} from './index.js'
 
-test('speakers', function (t) {
-  t.equal(speakers.eng, 322000000)
-  t.equal(speakers.jpn, 125000000)
-  t.equal(speakers.por, 182000000)
-  t.equal(speakers.cmn, 885000000)
-
-  t.end()
+test('speakers', function () {
+  assert.equal(speakers.eng, 322000000)
+  assert.equal(speakers.jpn, 125000000)
+  assert.equal(speakers.por, 182000000)
+  assert.equal(speakers.cmn, 885000000)
 })
